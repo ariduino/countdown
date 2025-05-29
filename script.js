23df9a91059f138aaeb3a4dea1f508c6bc1fc288
ariduino <89081982+ariduino@users.noreply.github.com>
2025-05-29 17:28:11 +0000

    Flashing color for the countdown

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ var x = setInterval(function() {
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
+  
   // Output the result in an element with id="countdown"
   document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
@@ -23,6 +23,12 @@ var x = setInterval(function() {
   // If the count down is over, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("countdown").innerHTML = "EXPIRED";
+    document.getElementById("countdown").innerHTML = "THE WORLD HAS AlREADY ENDED!!!";
+  }
+  if (second % 2 == 0) {
+    document.getElementById("countdown").style.color = "red";
+  }
+  else {
+    document.getElementById("countdown").style.color = "yellow";
   }
 }, 1000);
